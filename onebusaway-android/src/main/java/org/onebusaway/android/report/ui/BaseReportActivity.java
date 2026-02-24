669e264f4a88e1d7c1f712a52f92819860be32ae
Eden Gilbert <edengilbertus@proton.me>
2026-02-24 22:47:42 +0300

    Fix: Apply edge-to-edge window insets to report and feedback activities (Fix: #1474)

--- onebusaway-android/src/main/java/org/onebusaway/android/report/ui/BaseReportActivity.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/report/ui/BaseReportActivity.java
@@ -1,25 +1,27 @@
 /*
-* Copyright (C) 2014-2015 University of South Florida (sjbarbeau@gmail.com)
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2014-2015 University of South Florida (sjbarbeau@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.onebusaway.android.report.ui;
 
 import org.onebusaway.android.R;
+import org.onebusaway.android.util.UIUtils;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -39,6 +41,12 @@ import androidx.fragment.app.FragmentTransaction;
  * Created by Cagri Cetin
  */
 public class BaseReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UIUtils.setupActionBar(this);
+    }
 
     public static final String CLOSE_REQUEST = "BaseReportActivityClose";
 
@@ -87,7 +95,7 @@ public class BaseReportActivity extends AppCompatActivity {
         progressBar.getIndeterminateDrawable().setColorFilter(Color.WHITE, android.graphics.PorterDuff.Mode.MULTIPLY);
 
         ActionBar ab = getSupportActionBar();
-        if (ab != null){
+        if (ab != null) {
             ab.setDisplayShowCustomEnabled(true);
             ab.setCustomView(progressBar);
         }
@@ -109,7 +117,7 @@ public class BaseReportActivity extends AppCompatActivity {
 
     protected void addInfoText(String text) {
         // Instructions in header of report
-        if (mInfoHeader == null){
+        if (mInfoHeader == null) {
             mInfoHeader = (RelativeLayout) findViewById(R.id.ri_info_header);
         }
         ((TextView) mInfoHeader.findViewById(R.id.ri_info_text)).setText(text);
@@ -128,15 +136,15 @@ public class BaseReportActivity extends AppCompatActivity {
         }
     }
 
-    protected  boolean isInfoVisible() {
-        if (mInfoHeader == null){
+    protected boolean isInfoVisible() {
+        if (mInfoHeader == null) {
             mInfoHeader = (RelativeLayout) findViewById(R.id.ri_info_header);
         }
         return mInfoHeader.getVisibility() == View.VISIBLE;
     }
 
     protected void removeInfoText() {
-        if (mInfoHeader == null){
+        if (mInfoHeader == null) {
             mInfoHeader = (RelativeLayout) findViewById(R.id.ri_info_header);
         }
         ((TextView) mInfoHeader.findViewById(R.id.ri_info_text)).setText("");
